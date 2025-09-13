857647fd866fad30c3fb3551577d42c01db64420
Infamous003 <arfathsyed0312@gmail.com>
2025-09-13 16:24:35 +0530

    feat: add endpoints for retreiving, updating, and deleting users

---
+++ app/api/users.py
@@ -0,0 +1,71 @@
+from fastapi import APIRouter, status, HTTPException, Depends
+from ..schemas.user import UserCreate, UserPublic, UserUpdate
+from ..models.user import User
+from ..db.session import get_session
+from sqlmodel import Session, select
+from .deps import get_password_hash
+from .auth import get_current_user
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+
+@router.get("/", 
+            status_code=status.HTTP_200_OK, 
+            response_model=list[UserPublic])
+def get_user(session: Session = Depends(get_session)):
+    query = select(User)
+    user = session.exec(query).fetchall()
+
+    return user
+
+
+@router.get("/{id}", 
+            status_code=status.HTTP_200_OK, 
+            response_model=UserPublic)
+def get_user_by_id(id: int, 
+                   session: Session = Depends(get_session)):
+    query = select(User).where(User.id == id)
+    user = session.exec(query).one_or_none()
+
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    else:
+        return user
+    
+
+@router.put("/",
+            response_model=UserPublic,
+            status_code=status.HTTP_200_OK)
+def update_user(user: UserUpdate, 
+                session: Session = Depends(get_session),
+                current_user: User = Depends(get_current_user)):
+    query = select(User).where(User.id == current_user.id)
+    user_found = session.exec(query).one_or_none()
+
+    if user_found is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    if user.username: user_found.username = user.username
+    if user.firstname: user_found.firstname = user.firstname
+    if user.lastname: user_found.lastname = user.lastname
+    if user.password: user_found.password = get_password_hash(user.password)
+
+    session.add(user_found)
+    session.commit()
+    session.refresh(user_found)
+    return user_found
+
+
+@router.delete("/",
+               status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(session: Session = Depends(get_session),
+                current_user: User = Depends(get_current_user)):
+    query = select(User).where(User.id == current_user.id)
+
+    user_found = session.exec(query).one_or_none()
+
+    if user_found is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    else:
+        session.delete(user_found)
+        session.commit()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .db.session import init_db
 from contextlib import asynccontextmanager
-from .api import auth
+from .api import users, auth
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,6 +13,7 @@ app = FastAPI(title="LinkedIn Analytics API",
               lifespan=lifespan)
 
 app.include_router(auth.router)
+app.include_router(users.router)
 
 @app.get("/")
 def home():
