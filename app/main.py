d8dc0ddd3cc90b146242d094e9f8adaf9167f669
Infamous003 <arfathsyed0312@gmail.com>
2025-09-14 19:40:08 +0530

    chore: update lifespan event to start and stop the scheduler

--- app/main.py
+++ app/main.py
@@ -2,11 +2,18 @@ from fastapi import FastAPI
 from .db.session import init_db
 from contextlib import asynccontextmanager
 from .api import users, auth, posts
+from apscheduler.schedulers.background import BackgroundScheduler
+from .core.scheduler import publish_scheduled_posts
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     init_db()
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(publish_scheduled_posts, "interval", minutes=1)
+    scheduler.start()
     yield
+    scheduler.shutdown()
 
 app = FastAPI(title="LinkedIn Analytics API", 
               description="A backend API for LinkedIn",
