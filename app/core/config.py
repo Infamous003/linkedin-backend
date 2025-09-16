4a6e80a72e2d1ef31c69b0226d1d31e1bffbdfbe
Infamous003 <arfathsyed0312@gmail.com>
2025-09-16 19:41:38 +0530

    chore: add settings to include .env variables

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings
-
+from pydantic import computed_field
 
 class Settings(BaseSettings):
     DB_USERNAME: str = "postgres"
@@ -7,12 +7,23 @@ class Settings(BaseSettings):
     DB_NAME: str = "linkedindb"
     DB_HOST: str = "localhost"
     DB_PORT: int = 5432
-    DATABASE_URL: str = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-    
     SECRET_KEY: str = "this_is_a_secret"
     ALGORITHM: str = "HS256"
     TOKEN_EXPIRATION_MINUTES: int = 60
 
+    @computed_field 
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql://{self.DB_USERNAME}:{self.DB_PASSWORD}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
+
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
+
+print(settings.DB_USERNAME)   # -> from .env
+print(settings.DATABASE_URL)  # -> built from .env values
